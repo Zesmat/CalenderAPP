2c2afd82da9d75f8f335d2698e48b8b370fc7454
Zesmat <152190348+Zesmat@users.noreply.github.com>
2024-05-30 15:53:47 +0300

    moved to new folder package

---
+++ CalendarMain/CalendarApp.java
@@ -0,0 +1,15 @@
+package CalendarMain;
+
+
+class CalendarApp 
+{
+    private User user;
+
+    public CalendarApp(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
